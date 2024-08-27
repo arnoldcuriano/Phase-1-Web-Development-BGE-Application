00f7179ee6ab164eb17a33bf9b31479f460f81ed
Arnold C <78853864+arnoldcuriano@users.noreply.github.com>
2024-08-27 16:32:46 +0800

    Fixing the memoryStore error cause not suitable for production and Disable fileuploads and vercel

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const path = require('path');
+const fs = require('fs');
 
+// Initialize Express
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public')); // Serve static files from 'public' folder
+
+// Ensure 'uploads' directory exists
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 app.use('/uploads', express.static('uploads')); // Serve uploaded files from 'uploads' folder
 
 // Configurations
@@ -16,12 +25,22 @@ const config = require('./config/keys');
 require('./config/passport')(passport);
 
 // MongoDB Config
-mongoose.connect(config.mongoURI,);
+mongoose.connect(config.mongoURI, {
+
+});
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(session({ secret: config.secretOrKey, resave: true, saveUninitialized: true }));
+
+// Session Management with MongoStore
+app.use(session({
+    secret: config.secretOrKey,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: config.mongoURI })
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -31,11 +50,13 @@ app.use((req, res, next) => {
     next();
 });
 
+// View engine setup
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// Routes setup
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
 app.use('/dashboard', require('./routes/dashboard'));
@@ -49,11 +70,7 @@ app.use('/it/category-management', require('./routes/categories'));
 app.use('/clients', require('./routes/clients'));
 app.use('/hr-department', require('./routes/management'));
 app.use('/announcements', require('./routes/announcements'));
-app.use('/events', require('./routes/events')); 
-
-
-
-
+app.use('/events', require('./routes/events'));
 
 // Start server
 const PORT = process.env.PORT || 5000;
